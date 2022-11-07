6295596b4999b5b762c030877c076d16e6d39e75
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2022-11-06 19:15:23 -0700

    Create express server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("Server");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
