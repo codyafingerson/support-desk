b1b66a0c6e87914690443b3821a171f8bd745051
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2022-11-08 14:24:52 -0700

    Put & delete methods for tickets

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,9 +1,21 @@
-const express = require("express");
-const router = express.Router();
-const {getTickets, createTicket} = require("../controllers/ticketContoller");
+const express = require('express')
+const router = express.Router()
+const {
+  getTickets,
+  getTicket,
+  createTicket,
+  deleteTicket,
+  updateTicket,
+} = require('../controllers/ticketController')
 
-const {protect} = require("../middleware/authMiddleware");
+const { protect } = require('../middleware/authMiddleware')
 
-router.route('/').get(protect, getTickets).post(protect, createTicket);
+router.route('/').get(protect, getTickets).post(protect, createTicket)
 
-module.exports = router;
+router
+  .route('/:id')
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket)
+
+module.exports = router
