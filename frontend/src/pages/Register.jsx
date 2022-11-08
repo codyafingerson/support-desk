5f712bf180d2e4ac0515711a97fb06b91e0a42f0
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2022-11-08 13:35:15 -0700

    Add register user functionality

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import { FaUser } from "react-icons/fa";
+import {useNavigate} from "react-router-dom";
 
 // Redux features
 import {useSelector, useDispatch} from "react-redux";
-import {register} from "../features/auth/authSlice";
+import {register, reset} from "../features/auth/authSlice";
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -17,8 +18,21 @@ function Register() {
   const { name, email, password, password2 } = formData;
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const {user, isLoading, isSuccess, message} = useSelector(state => state.auth);
+  const {user, isLoading, isSuccess, isError, message} = useSelector(state => state.auth);
+
+  useEffect(() => {
+    if(isError) {
+      toast.error(message);
+    }
+    // Redirect when logged in
+    if(isSuccess || user) {
+      navigate('/');
+    }
+
+    dispatch(reset());
+  }, [isError, isSuccess, user, message, navigate, dispatch]);
 
   const onChange = (e) => {
     setFormData((prevState) => ({
